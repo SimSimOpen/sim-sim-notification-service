af72c49d1a085376867360052577131e64a8b2f1
jimmy <jimishukurow@gmail.com>
2026-03-04 16:15:04 +0500

    RabbitMq Congiuration updated

--- src/main/java/info/jemsit/notification_service/config/RabbitMQConfig.java
+++ src/main/java/info/jemsit/notification_service/config/RabbitMQConfig.java
@@ -1,12 +1,15 @@
 package info.jemsit.notification_service.config;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import static info.jemsit.common.data.constants.RabbitMQConstants.MEDIA_QUEUE;
+import static info.jemsit.common.data.constants.RabbitMQConstants.*;
 
 
 @Configuration
@@ -15,6 +18,16 @@ public class RabbitMQConfig {
     @Bean
     public Queue mediaQueue() {
         return new Queue(MEDIA_QUEUE, true);
+    }
+
+    @Bean
+    public TopicExchange mediaExchange() {
+        return new TopicExchange(MEDIA_EXCHANGE);
+    }
+
+    @Bean
+    public Binding mediaBinding(Queue mediaQueue, TopicExchange mediaExchange) {
+        return BindingBuilder.bind(mediaQueue).to(mediaExchange).with(MEDIA_KEY);
     }
 
     @Bean
